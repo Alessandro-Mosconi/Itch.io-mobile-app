49b3d39dbbbdbec9ab462c3fcfab6e32852630c2
Alessandro-Mosconi <alessandro2.mosconi@mail.polimi.it>
2024-05-07 15:58:26 +0200

    item_list firebase function created

--- firebase_backend/functions/index.js
+++ firebase_backend/functions/index.js
@@ -40,3 +40,110 @@ exports.fetchRSSFeedAndNotify = functions.https.onRequest(async (request, respon
         response.status(500).send('Failed to fetch RSS feed or send notification');
     }
 });
+
+exports.item_list = functions.https.onRequest(async (request, response) => {
+    const xml2js = require('xml2js');
+    const rssUrl = 'https://itch.io/';
+
+    // Verifica che il metodo della richiesta sia POST
+    if (request.method !== "GET") {
+        response.status(400).send('Please send a GET request');
+        return;
+    }
+
+    // Verifica che il corpo della richiesta contenga una stringa
+    if (!request.body) {
+        response.status(400).send('Please provide a the request body');
+        return;
+    }
+    if (request.body.filters && typeof request.body.filters !== 'string') {
+        response.status(400).send('Please provide a correct filters filed type in request body');
+        return;
+    }
+
+    if(!request.body.type ||  typeof request.body.type !== 'string') {
+        response.status(400).send('Please provide an type field in the request body');
+        return;
+    }
+
+    // Esegui le elaborazioni sulla stringa di input
+    const filters = request.body.filters || '';
+    const type = request.body.type;
+
+    const filteredUrl = rssUrl + type  + filters + '.xml'
+    console.log(filteredUrl)
+
+    let items = [];
+    let title = '';
+    
+    try {
+        // Ottieni il feed RSS con fetch
+        const res = await fetch(filteredUrl);
+        const xmlData = await res.text();
+
+        // Opzioni per il parsing
+        const parserOptions = {
+            trim: true,
+            explicitArray: false
+        };
+
+        // Effettua il parsing del documento XML
+        xml2js.parseString(xmlData, parserOptions, (err, result) => {
+            if (err) {
+                response.status(400).send('Errore nel parsing del documento XML:', err);
+                return;
+            }
+
+            items = result.rss.channel.item;            
+            title = result.rss.channel.title
+
+        });
+    } catch (error) {
+        response.status(400).send('Errore nella connessione con itch.io o url non corretto: "' + filteredUrl + '"\n' + error);
+    }
+
+    // Correzione dei vari parametri di ogni gioco
+    for(const game of items){
+        const altRegex = /alt="(.*?)"/;
+        // Espressione regolare per estrarre il valore dell'attributo src
+        const srcRegex = /src="(.*?)"/;
+
+        // Estrarre il testo tra le doppie virgolette per l'attributo alt
+        const altMatch = altRegex.exec(game.description);
+        const alt = altMatch ? altMatch[1] : '';
+
+        // Estrarre il valore dell'attributo src
+        const srcMatch = srcRegex.exec(game.description);
+        const img = srcMatch ? srcMatch[1] : '';
+
+        // Rimuovere il tag img dalla descrizione
+        const cleanDescription = game.description.replace(/<img.*?>/, '').trim();
+        
+        game.alt = alt;
+        game.img = img;
+        game.oldDescription = game.description;
+        game.description = cleanDescription;
+
+        const [titleText, filtersText] = game.title.split(" [");
+
+        const filters = filtersText.replace(/\]/g, '').split(' ').filter(filter => filter !== '');
+
+        game.title = titleText;
+        game.filters = filters;
+
+        game.platforms = Object.keys(game.platforms).filter(key => game.platforms[key] === "yes");
+
+    }
+
+    // Crea un oggetto JSON con i risultati
+    const result = {
+        parsed_url: filteredUrl,
+        title: title,
+        item_type: type,
+        items_size: items.length,
+        items: items
+    };
+
+    // Restituisci l'oggetto JSON come risposta
+    response.json(result);
+});
