479ba326e02555451f7e3cecd3e89c2d5b23c68c
Martino <72280379+martinopiaggi@users.noreply.github.com>
2024-06-04 11:28:56 +0200

    add responsive grid-list to favorite page

--- itchio/lib/views/favorite_page.dart
+++ itchio/lib/views/favorite_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../helperClasses/Game.dart';
 import '../helperClasses/Jam.dart';
 import '../providers/favorite_provider.dart';
 import '../widgets/custom_app_bar.dart';
@@ -20,6 +21,7 @@ class _FavoritePageState extends State<FavoritePage> with SingleTickerProviderSt
     _tabController = TabController(length: 2, vsync: this);
     _fetchFavorites();
   }
+
   Future<void> _fetchFavorites() async {
     await Future.wait([
       Provider.of<FavoriteProvider>(context, listen: false).fetchFavoriteGames(),
@@ -56,16 +58,11 @@ class _FavoritePageState extends State<FavoritePage> with SingleTickerProviderSt
                 if (favoriteProvider.favoriteGames.isEmpty)
                   Center(child: Text('Nessun gioco preferito ancora'))
                 else
-                  ListView.builder(
-                    itemCount: favoriteProvider.favoriteGames.length,
-                    itemBuilder: (context, index) {
-                      return GameCard(game: favoriteProvider.favoriteGames[index]);
-                    },
-                  ),
+                  _buildGameGrid(favoriteProvider.favoriteGames, context),
                 if (favoriteProvider.favoriteJams.isEmpty)
                   Center(child: Text('Nessuna jam preferita ancora'))
                 else
-                  _buildJamList(favoriteProvider.favoriteJams),
+                  _buildJamGrid(favoriteProvider.favoriteJams, context),
               ],
             );
           },
@@ -74,17 +71,57 @@ class _FavoritePageState extends State<FavoritePage> with SingleTickerProviderSt
     );
   }
 
-  ListView _buildJamList(List<Jam> jams) {
-    return ListView.builder(
+  GridView _buildGameGrid(List<Game> games, BuildContext context) {
+    double aspectRatio = _getChildAspectRatio(context);
+    int crossAxisCount = MediaQuery.of(context).size.width > 600 ? 3 : 1; //# columns
+
+    return GridView.builder(
+      padding: EdgeInsets.all(8),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: aspectRatio,
+      ),
+      itemCount: games.length,
+      itemBuilder: (context, index) {
+        return GameCard(game: games[index]);
+      },
+    );
+  }
+
+  GridView _buildJamGrid(List<Jam> jams, BuildContext context) {
+    double aspectRatio = _getChildAspectRatio(context);
+    int crossAxisCount = MediaQuery.of(context).size.width > 600 ? 3 : 1; //# columns
+
+    return GridView.builder(
+      padding: EdgeInsets.all(8),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: aspectRatio,
+      ),
       itemCount: jams.length,
       itemBuilder: (context, index) {
         return JamCard(
           jam: jams[index],
-          onAddToCalendar: () => {}//_addToCalendar(context, jams[index]),
+          onAddToCalendar: () => {}, // _addToCalendar(context, jams[index]),
         );
       },
     );
   }
 
+  double _getChildAspectRatio(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    Orientation orientation = MediaQuery.of(context).orientation;
 
+    if (width > 1200) {
+      return orientation == Orientation.landscape ? 1.5 : 1;
+    } else if (width > 600) {
+      return orientation == Orientation.landscape ? 1.5 : 1.2;
+    } else {
+      return 1.5;
+    }
+  }
 }
