96cf6bb34386c2504611b1c93d2b4bb4c6a2fda9
Alessandro-Mosconi <alessandro2.mosconi@mail.polimi.it>
2024-06-06 21:25:24 +0200

    fix init first page

--- itchio/lib/views/auth_or_main_view.dart
+++ itchio/lib/views/auth_or_main_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 
 import '../services/oauth_service.dart';
@@ -6,14 +7,32 @@ import '../views/auth_page.dart';
 import '../views/main_view.dart';
 
 class AuthOrHomePage extends StatelessWidget {
+  final Logger logger = Logger(
+    printer: PrettyPrinter(),
+  );
   @override
   Widget build(BuildContext context) {
-    final authService = Provider.of<OAuthService>(context);
+    final authService = Provider.of<OAuthService>(context, listen: false);
 
-    if (authService.accessToken == null) {
-      return AuthPage();
-    } else {
-      return MainView();
-    }
+    return FutureBuilder(
+      future: authService.init(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(child: Text('An error occured while initializing: ${snapshot.error}')),
+          );
+        } else {
+          if (authService.accessToken == null) {
+            return AuthPage();
+          } else {
+            return MainView();
+          }
+        }
+      },
+    );
   }
 }
