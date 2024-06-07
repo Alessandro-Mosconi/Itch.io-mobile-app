473805278299b37e20cb1206f6e3a0decc39e5e3
Martino <72280379+martinopiaggi@users.noreply.github.com>
2024-06-07 02:20:44 +0200

    removed useless loggerr

--- itchio/lib/services/notification_service.dart
+++ itchio/lib/services/notification_service.dart
@@ -1,10 +1,9 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:logger/logger.dart';
 
 class NotificationService {
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
-  final Logger logger = Logger(printer: PrettyPrinter());
+
   NotificationService(this.flutterLocalNotificationsPlugin);
 
   Future<void> setupNotifications() async {
